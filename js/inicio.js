01c6c50a90d9c478bb430cf61fb45256a2ae17ca
PabloAvelar <pabloarmenta84@gmail.com>
2023-08-23 19:00:20 -0600

    making the login frame a little more higher

--- js/inicio.js
+++ js/inicio.js
@@ -97,7 +97,7 @@ const styles = StyleSheet.create({
         borderBottomEndRadius: 10,
         borderBottomStartRadius: 10,
         width: '100%',
-        height: '40%',
+        height: '45%',
         alignItems: 'center',
         justifyContent: 'center',
     },
