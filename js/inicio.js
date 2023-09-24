87222c9f78224da9844ad40bdfe156e6956ea944
PabloAvelar <pabloarmenta84@gmail.com>
2023-09-24 13:26:51 -0600

    Registros y Consultas a la base de datos. Apliqué las validaciones a los textinput y un boton de cerrar modal (X)

--- js/inicio.js
+++ js/inicio.js
@@ -3,69 +3,128 @@
 // Paleta de color que estoy usando: https://paletadecolores.com.mx/paleta/f8a834/e19527/ca821a/b46f0d/9d5c00/
 
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput, Modal, Alert } from 'react-native';
 
 export default class Inicio extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal_window: false,
+      email: '',
+      password: ''
     };
   }
 
-  
   render() {
     const facebook = () => {
       console.log('Iniciar sesión con facebook');
+      this.setState({ modal_window: true });
     }
 
     const email = () => {
       console.log('Iniciar sesión con email');
+      this.setState({ modal_window: true });
     }
 
-    const toSignUp = () =>{
+    const toSignUp = () => {
       this.props.navigation.navigate('Inscripcion');
+    }
+
+    const closeModal = () => {
+      if (this.state.email.trim() === '' || this.state.password.trim() === '') {
+        Alert.alert('Campos requeridos', 'Llena todo los campos del formulario.');
+        return
+      }
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          // Typical action to be performed when the document is ready:
+          console.log(xhttp.responseText);
+          if (xhttp.responseText === '0') {
+            Alert.alert(
+              'Ha ocurrido un error :(',
+              'La contraseña es incorrecta. Intenta de nuevo.'
+            );
+          } else if (xhttp.responseText === '3') {
+            Alert.alert(
+              'Ha ocurrido un error :(',
+              'Cuenta no existente. ¡Crea una cuenta con nosotros!'
+            );
+          }
+        }
+      };
+
+      // Verificando desde la base de datos
+      xhttp.open("GET", `https://avelararmenta.000webhostapp.com/verifica.php?correo=${this.state.email}&contraseña=${this.state.password}`, true);
+      xhttp.send();
+
+      // Regresando los valores a su estado original 
+      this.state.modal_window = false;
+      this.state.email = ''
+      this.state.password = ''
+
+      this.setState({ modal_window: false });
+
     }
 
     return (
       <View style={styles.root}>
         <View style={[styles.container, styles.shadow]}>
-            <Image
-                style={styles.logo}
-                source={require('../images/inge.png')}
-            />
-            <View style={styles.login}>
+          <Image
+            style={styles.logo}
+            source={require('../images/inge.png')}
+          />
+          <View style={styles.login}>
             <Text style={styles.txtTitle}>Welcome to the HandyMan</Text>
-                <View style={styles.buttons}>
+            <View style={styles.buttons}>
 
-                <TouchableOpacity style={styles.clickable_btn} onPress={facebook} >
-                  <View style={[styles.btn, styles.shadow, styles.facebook]}>
-                    <Image
-                      style={styles.icon}
-                      source={require('../images/icon_fb.png')}
-                    />
-                    <Text style={[styles.box, {color: color_txt}]}>Log in with Facebook</Text>
-                  </View>
-
-                </TouchableOpacity>
-                  
-                  <TouchableOpacity style={styles.clickable_btn} onPress={email}>
-                    <View style={[styles.btn, styles.shadow, styles.email]}>
-                      <Image
-                        style={styles.icon}
-                        source={require('../images/icon_email.png')}
-                      />
-                      <Text style={[styles.box, {color: color_txt}]}>Log in with Email</Text>
-                    </View>
-                  </TouchableOpacity>
-                  <View>
-                    <TouchableOpacity onPress={toSignUp}>
-                      <Text>Don't have an account? <Text style={[styles.hyperlink, {color: color_borders, textDecorationLine: 'underline'}]}>Sign up</Text> </Text>
-                    </TouchableOpacity>
-                  </View>
+              <TouchableOpacity style={styles.clickable_btn} onPress={facebook} >
+                <View style={[styles.btn, styles.shadow, styles.facebook]}>
+                  <Image
+                    style={styles.icon}
+                    source={require('../images/icon_fb.png')}
+                  />
+                  <Text style={[styles.box, { color: color_txt }]}>Log in with Facebook</Text>
                 </View>
 
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.clickable_btn} onPress={email}>
+                <View style={[styles.btn, styles.shadow, styles.email]}>
+                  <Image
+                    style={styles.icon}
+                    source={require('../images/icon_email.png')}
+                  />
+                  <Text style={[styles.box, { color: color_txt }]}>Log in with Email</Text>
+                </View>
+              </TouchableOpacity>
+              <View>
+                <TouchableOpacity onPress={toSignUp}>
+                  <Text>Don't have an account? <Text style={[styles.hyperlink, { color: color_borders, textDecorationLine: 'underline' }]}>Sign up</Text> </Text>
+                </TouchableOpacity>
+              </View>
             </View>
+          </View>
         </View>
+
+        <Modal transparent={false} visible={this.state.modal_window} animationType='fade'>
+          <View style={[styles.modal, styles.shadow]}>
+            <TouchableOpacity style={styles.ExitModal} onPress={() => {
+              console.log('Cerando modal: X');
+              this.setState({ modal_window: false });
+            }}>
+              <Text style={{ color: "white", fontSize: 20 }}>x</Text>
+            </TouchableOpacity>
+            <View style={styles.form}>
+              <TextInput placeholder='Email' placeholderTextColor={'white'} style={styles.textfield} onChangeText={email => this.setState({ email })} />
+              <TextInput placeholder='Password' placeholderTextColor={'white'} style={styles.textfield} onChangeText={password => this.setState({ password })} />
+            </View>
+            <TouchableOpacity style={[styles.btn, styles.create_btn, styles.shadow, { color: 'white' }]} onPress={closeModal}>
+              <Text>Log in</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -80,90 +139,133 @@ const bg_email = '#333';
 const color_borders = '#955903';
 
 const styles = StyleSheet.create({
-    clickable_btn:{
-      width: 275,
-      height: 60,
-      alignItems: 'center',
-    },
 
-    root:{
-        height: '100%',
-        backgroundColor: "#fff",
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  ExitModal: {
+    position: 'absolute',
+    right: 5,
+    top: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,
+    width: 45,
+    height: 45,
 
-    shadow:{
-        shadowColor: '#171717',
-        shadowOffset: {width: -2, height:4},
-        shadowOpacity: 0.5,
-        shadowRadius: 3,
-        elevation: 5,
-    },
+  },
 
-    container:{
-        height: '92%',
-        width: '85%',
-        backgroundColor: bg_mainFrame,
-        borderRadius: 12,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        // borderColor: color_borders,
-    },
+  form: {
+    width: '80%',
+    height: '60%',
+    justifyContent: 'space-evenly'
+  },
 
-    logo:{
-        marginTop: '13%',
-        width: 300,
-        height: 300,
-    },
+  modal: {
+    backgroundColor: "#333",
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: '90%',
+    height: "50%",
 
-    icon:{
-      width: 20,
-      height: 20,
-      marginRight: '10%',
-      marginLeft: '10%',
-    },
+    marginVertical: '50%',
+    marginHorizontal: '5%',
+    borderRadius: 13,
+  },
 
-    login:{
-        backgroundColor: bg_login,
-        borderColor: "#E59729",
-        borderBottomEndRadius: 10,
-        borderBottomStartRadius: 10,
-        width: '100%',
-        height: '45%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        
-    },
+  create_btn: {
+    color: '#ddd',
+    width: 210,
+    height: 50,
+    fontSize: 20,
+    backgroundColor: '#202020',
+    justifyContent: 'center',
+  },
 
-    txtTitle:{
-      color: color_txt,
-        padding: '5%',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
+  textfield: {
+    borderBottomWidth: 2,
+  },
+  clickable_btn: {
+    width: 275,
+    height: 60,
+    alignItems: 'center',
+  },
 
-    buttons:{
-      alignItems: 'center',
-      justifyContent: 'space-evenly',
-      height: '70%',
-    },
+  root: {
+    height: '100%',
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-    btn:{
-      width: 275,
-      height: 60,
-      padding: 5,
-      borderRadius: 30,
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-    },
+  shadow: {
+    shadowColor: '#171717',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 5,
+  },
 
-    facebook:{
-      backgroundColor: bg_fb,
-    },
+  container: {
+    height: '92%',
+    width: '85%',
+    backgroundColor: bg_mainFrame,
+    borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    // borderColor: color_borders,
+  },
 
-    email:{
-      backgroundColor: bg_email,
-    }
+  logo: {
+    marginTop: '13%',
+    width: 300,
+    height: 300,
+  },
+
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: '10%',
+    marginLeft: '10%',
+  },
+
+  login: {
+    backgroundColor: bg_login,
+    borderColor: "#E59729",
+    borderBottomEndRadius: 10,
+    borderBottomStartRadius: 10,
+    width: '100%',
+    height: '45%',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+  txtTitle: {
+    color: color_txt,
+    padding: '5%',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  buttons: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    height: '70%',
+  },
+
+  btn: {
+    width: 275,
+    height: 60,
+    padding: 5,
+    borderRadius: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+
+  facebook: {
+    backgroundColor: bg_fb,
+  },
+
+  email: {
+    backgroundColor: bg_email,
+  }
 });
--- js/inscripcion.js
+++ js/inscripcion.js
@@ -1,101 +1,146 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Modal, TextInput, StyleSheet, Image} from 'react-native';
+import { View, Text, TouchableOpacity, Modal, TextInput, StyleSheet, Image } from 'react-native';
 
 export default class Inscripcion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        modal_window:false,
-    };
-}
-
-render() {
-    const bg_fb = '#3b5998';
-    const bg_email = '#333';
-    const ph_color = "DDD"
-
-    const facebook = () =>{
-        this.setState({modal_window:true});
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal_window: false,
+            name: '',
+            email: '',
+            password: ''
+        };
     }
 
-    const email = () => {
-        this.setState({modal_window:true});
-    }
-    const closeModal = () => {
-        this.setState({modal_window:false});
-    }
+    render() {
+        const bg_fb = '#3b5998';
+        const bg_email = '#333';
+        const ph_color = "DDD"
 
-    const toLogIn = () =>{
-        this.props.navigation.navigate('Inicio');
-    }
+        const facebook = () => {
+            this.setState({ modal_window: true });
+        }
 
-    return (
-    <View style={styles.root}>
-        <View style={[styles.container, styles.shadow]}>
-            <View style={{height: '13%', alignItems: 'center', justifyContent:'space-between'}}>
-                <Text style={styles.title}>Create an account</Text>
-                <Text style={styles.subtitle}>Join today.</Text>
-            </View>
+        const email = () => {
+            this.setState({ modal_window: true });
+        }
 
-            <View style={styles.buttons_area}>
-                <TouchableOpacity onPress={facebook} style={[styles.btn, styles.shadow, {backgroundColor: bg_fb}]}>
-                    <Image
-                        style={styles.icon}
-                        source={require('../images/icon_fb.png')}
-                      />
-                    <Text style={{color: '#fff'}}>Continue with Facebook</Text>
-                </TouchableOpacity>
+        const closeModal = () => {
+            // Comprobando que todos los campos estén llenos
+            if (this.state.email.trim() === '' || this.state.password.trim() === '' || this.state.name.trim() === '') {
+                Alert.alert('Campos requeridos', 'Llena todo los campos del formulario.');
+                return
+            }
 
-                <TouchableOpacity onPress={email} style={[styles.btn, styles.shadow, {backgroundColor: bg_email}]} >
-                    <Image
-                        style={styles.icon}
-                        source={require('../images/icon_email.png')}
-                      />
-                    <Text style={{color: '#fff'}}>Continue with Email</Text>
-                </TouchableOpacity>
-            </View>
-            <TouchableOpacity onPress={toLogIn}>
-                <View style={styles.footer}>
-                    <Text style={{color: '#000'}}>Already have an account? <Text style={{color: "#955903", textDecorationLine: 'underline'}}>Log in</Text></Text>
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    // Typical action to be performed when the document is ready:
+                    console.log(xhttp.responseText);
+                }
+            };
+
+            // Haciendo el registro en la base de datos
+            xhttp.open("GET", `https://avelararmenta.000webhostapp.com/datos.php?nombre=${this.state.name}&correo=${this.state.email}&contraseña=${this.state.password}`, true);
+            xhttp.send();
+
+            // Regresando los valores a su estado original 
+            this.state.modal_window = false;
+            this.state.email = ''
+            this.state.password = ''
+
+            this.setState({ modal_window: false });
+        }
+
+        const toLogIn = () => {
+            this.props.navigation.navigate('Inicio');
+        }
+
+        return (
+            <View style={styles.root}>
+                <View style={[styles.container, styles.shadow]}>
+                    <View style={{ height: '13%', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Text style={styles.title}>Create an account</Text>
+                        <Text style={styles.subtitle}>Join today.</Text>
+                    </View>
+
+                    <View style={styles.buttons_area}>
+                        <TouchableOpacity onPress={facebook} style={[styles.btn, styles.shadow, { backgroundColor: bg_fb }]}>
+                            <Image
+                                style={styles.icon}
+                                source={require('../images/icon_fb.png')}
+                            />
+                            <Text style={{ color: '#fff' }}>Continue with Facebook</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={email} style={[styles.btn, styles.shadow, { backgroundColor: bg_email }]} >
+                            <Image
+                                style={styles.icon}
+                                source={require('../images/icon_email.png')}
+                            />
+                            <Text style={{ color: '#fff' }}>Continue with Email</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <TouchableOpacity onPress={toLogIn}>
+                        <View style={styles.footer}>
+                            <Text style={{ color: '#000' }}>Already have an account? <Text style={{ color: "#955903", textDecorationLine: 'underline' }}>Log in</Text></Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
-            </TouchableOpacity>
-        </View>
 
-        <Modal transparent={false} visible={this.state.modal_window} animationType="fade">
-            <View style={[styles.modal, styles.shadow]}>
-                <View style={styles.form}>
-                    <TextInput placeholder='Name' placeholderTextColor={'white'} style={styles.textfield}/>
+                <Modal transparent={false} visible={this.state.modal_window} animationType="fade">
+                    <View style={[styles.modal, styles.shadow]}>
+                        <TouchableOpacity style={styles.ExitModal} onPress={() => {
+                            console.log('Cerando modal: X');
+                            this.setState({ modal_window: false });
+                        }}>
+                            <Text style={{ color: "white", fontSize: 20 }}>x</Text>
+                        </TouchableOpacity>
+                        <View style={styles.form}>
+                            <TextInput placeholder='Name' placeholderTextColor={'white'} style={styles.textfield} onChangeText={name => this.setState({ name })} />
 
-                    <TextInput placeholder='Email' placeholderTextColor={'white'} style={styles.textfield}/>
+                            <TextInput placeholder='Email' placeholderTextColor={'white'} style={styles.textfield} onChangeText={email => this.setState({ email })} />
 
-                    <TextInput placeholder='Password' placeholderTextColor={'white'} style={styles.textfield}/>
+                            <TextInput placeholder='Password' placeholderTextColor={'white'} style={styles.textfield} secureTextEntry={true} onChangeText={password => this.setState({ password })} />
 
-                </View>
+                        </View>
 
-                <TouchableOpacity style={[styles.btn, styles.create_btn, styles.shadow, {color: 'white'}]} onPress={closeModal}>
-                    <Text>Create</Text>
-                </TouchableOpacity>
+                        <TouchableOpacity style={[styles.btn, styles.create_btn, styles.shadow, { color: 'white' }]} onPress={closeModal}>
+                            <Text>Create</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
+
             </View>
-        </Modal>
-
-    </View>
-    );
-  }
+        );
+    }
 }
 const bg_root = '#faac3c';
 
 const styles = StyleSheet.create({
-    textfield:{
+    ExitModal: {
+        position: 'absolute',
+        right: 5,
+        top: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 5,
+        width: 45,
+        height: 45,
+
+    },
+
+    textfield: {
         borderBottomWidth: 2,
     },
 
-    form:{
+    form: {
         width: '80%',
         height: '60%',
         justifyContent: 'space-between'
     },
 
-    modal:{
+    modal: {
         backgroundColor: "#333",
         alignItems: 'center',
         justifyContent: 'space-evenly',
@@ -107,7 +152,7 @@ const styles = StyleSheet.create({
         borderRadius: 13,
     },
 
-    create_btn:{
+    create_btn: {
         color: '#ddd',
         width: 210,
         height: 50,
@@ -116,14 +161,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
-    root:{
+    root: {
         backgroundColor: bg_root,
         height: '100%',
         alignItems: 'center',
         justifyContent: 'center',
     },
 
-    container:{
+    container: {
         backgroundColor: '#fff',
         color: '#000',
         height: '80%',
@@ -133,40 +178,40 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
     },
 
-    shadow:{
+    shadow: {
         shadowColor: '#171717',
-        shadowOffset: {width: -2, height:4},
+        shadowOffset: { width: -2, height: 4 },
         shadowOpacity: 0.5,
         shadowRadius: 3,
         elevation: 5,
     },
 
-    icon:{
+    icon: {
         width: 20,
         height: 20,
         marginRight: '10%',
         marginLeft: '10%',
-      },
+    },
 
-    title:{
+    title: {
         color: '#000',
         fontSize: 25,
         fontWeight: 'bold',
     },
 
-    subtitle:{
+    subtitle: {
         color: '#000',
         fontSize: 18,
     },
 
-    buttons_area:{
+    buttons_area: {
         width: '100%',
         height: '25%',
         alignItems: 'center',
         justifyContent: 'space-between',
     },
 
-    btn:{
+    btn: {
         color: '#000',
         width: 275,
         height: 60,
